77edf3591f8be17879eb6ceeeec672c36c56b8f3
Reza Khodadadi <rezakho@gmail.com>
2015-07-10 20:11:15 +0430

    Updated gateway interface

--- src/Payment/Gateway/GatewayInterface.php
+++ src/Payment/Gateway/GatewayInterface.php
@@ -4,4 +4,23 @@ namespace Payment\Gateway;
 
 interface GatewayInterface
 {
+	public function send($amount, $receiptId);
+
+	public function redirect();
+
+	public function getToken();
+
+	public function getRequestData();
+
+	public function getRequestError();
+
+	public function captureResponse();
+
+	public function isResponseOk();
+
+	public function getResponseData();
+
+	public function getResponseError();
+
+	public function verify($orderId, $saleReferenceId);
 }

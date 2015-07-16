aa59947f3915563e734c313e89099a85b94b139d
Reza Khodadadi <rezakho@gmail.com>
2015-07-16 20:05:43 +0430

    Added simple test

---
+++ tests/SamanTest.php
@@ -0,0 +1,12 @@
+<?php
+
+// sample of phpunit web service mocking
+// https://phpunit.de/manual/current/en/test-doubles.html#test-doubles.stubbing-and-mocking-web-services.examples.GoogleTest.php
+
+class SamanTest extends PHPUnit_Framework_TestCase
+{
+	public function testFirst()
+	{
+		$this->assertEquals(true, true);
+	}
+}
